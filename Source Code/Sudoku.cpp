b374ceedf3351d40f47abbadead1404fbf30e17a
Ali-Rehan <alirehan2152@gmail.com>
2025-11-21 09:42:26 +0500

    Completed sudoku generation code. Issue is there is a 0.00000000000000000000000000000000000001% chance of it being generated correctly.

--- Source Code/Sudoku.cpp
+++ Source Code/Sudoku.cpp
@@ -7,12 +7,17 @@ void generatesolution(int array[9][9]);
 bool columnvalid(int array[9][9], int x, int y);
 bool gridvalid(int array[9][9], int x, int y);
 bool rowvalid(int array[9][9], int x, int y);
+void display(int array[9][9]);
 
 void main() {
 	srand(time(0));
 	int board[9][9], solution[9][9];
 
 	initialize(board, solution);
+
+	generatesolution(solution);
+
+	display(solution);
 
 
 }
@@ -30,19 +35,38 @@ void generatesolution(int array[9][9]) {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			do {
-				array[i][j] = rand() % 10 + 1;
-			} while (gridvalid(array,i,j) && columnvalid(array,i,j) && rowvalid(array,i,j));
+				array[i][j] = rand() % 9 + 1;
+			} while (!gridvalid(array,i,j) || !columnvalid(array,i,j) || !rowvalid(array,i,j));
+			std::cout << array[i][j] << " ";
 		}
+		std::cout << std::endl;
 	}
 }
 
 bool columnvalid(int array[9][9], int x, int y) {
-
+	int row = x;
+	while (row != 0) {//goes to the end of the column
+		row--;
+	}
+	while (row != 9) {
+		if (row == x) {//skips row checking if the indices match
+			row++;
+			continue;
+		}
+		else {//checks if a duplicate exists
+			if (array[row][y] == array[x][y]) {
+				return 0;
+			}
+			else {
+				row++;
+			}
+		}
+	}
+	return 1;
 }
 
 bool gridvalid(int array[9][9], int x, int y) {
-	int row = x, column = y;
-
+	int row = x, column = y, counter=0;
 	//takes the row and column to the start of the 3x3 grid
 	while (row % 3 != 0)
 		row--;
@@ -50,38 +74,44 @@ bool gridvalid(int array[9][9], int x, int y) {
 		column--;
 
 	//runs until column goes out of 3x3 grid
-	do {
-		if (x == row && y == column) {//does not compare the grid position if it is same as the position to be validated
-			row++;
-			if (row % 3 == 0) {
-				row - 3;
-				column++;
-				if (column % 3 == 0) {
-					break;
-				}
-			}
-			continue;
-		}
-		else {//checks if the current grid position duplicates the grid position to be validated
-			if (array[row][column] == array[x][y]) {
+	for (int i = row; i < row + 3; i++) {
+		for (int j = column; j < column + 3; j++) {
+			if (i == x && j == y)
+				continue;
+			if (array[i][j] == array[x][y])
 				return 0;
-			}
-			else {
-				row++;
-				if (row % 3 == 0) {
-					row - 3;
-					column++;
-					if (column % 3 == 0) {
-						break;
-					}
-				}
-			}
 		}
-	} while (column % 3 != 0);
-
+	}
 	return 1;
 }
 
 bool rowvalid(int array[9][9], int x, int y) {
+	int column=y;
+	while (column != 0) {//goes to the end of the row
+		column--;
+	}
+	while (column != 9) {//traverses from start to end of row
+		if (column == y) {//skips checking if the indices match
+			column++;
+			continue;
+		}
+		else {//checks if a duplicate exists on the index position
+			if (array[x][column] == array[x][y]) {
+				return 0;
+			}
+			else {
+				column++;
+			}
+		}
+	}
+	return 1;
+}
 
+void display(int array[9][9]) {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			std::cout << array[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
 }
